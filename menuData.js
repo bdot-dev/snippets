6d5b472198b30c751925142dab7da59fe45e2672
baaayh <baaaayh@gmail.com>
2024-09-09 18:29:51 +0900

    first commit

---
+++ menuData.js
@@ -0,0 +1,34 @@
+export default {
+    mainMenu: [
+        { title: 'Menu1', url: '/ko/menu1/index1.html' },
+        { title: 'Menu2', url: '/ko/menu2/index1.html' },
+        { title: 'Menu3', url: '/ko/menu3/index1.html' },
+        { title: 'Menu4', url: '/ko/menu4/index1.html' },
+    ],
+    subMenu: {
+        Menu1: [
+            { title: 'index1', url: '/ko/menu1/index1.html' },
+            { title: 'index2', url: '/ko/menu1/index2.html' },
+            { title: 'index3', url: '/ko/menu1/index3.html' },
+            { title: 'index4', url: '/ko/menu1/index4.html' },
+        ],
+        Menu2: [
+            { title: 'index1', url: '/ko/menu2/index1.html' },
+            { title: 'index2', url: '/ko/menu2/index2.html' },
+            { title: 'index3', url: '/ko/menu2/index3.html' },
+            { title: 'index4', url: '/ko/menu2/index4.html' },
+        ],
+        Menu3: [
+            { title: 'index1', url: '/ko/menu3/index1.html' },
+            { title: 'index2', url: '/ko/menu3/index2.html' },
+            { title: 'index3', url: '/ko/menu3/index3.html' },
+            { title: 'index4', url: '/ko/menu3/index4.html' },
+        ],
+        Menu4: [
+            { title: 'index1', url: '/ko/menu4/index1.html' },
+            { title: 'index2', url: '/ko/menu4/index2.html' },
+            { title: 'index3', url: '/ko/menu4/index3.html' },
+            { title: 'index4', url: '/ko/menu4/index4.html' },
+        ],
+    },
+};
