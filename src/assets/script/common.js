3dbfd5c59136a4ace3da8058418baa1f8c3464ea
baaayh <baaaayh@gmail.com>
2024-09-20 18:28:30 +0900

    svg

--- src/assets/script/common.js
+++ src/assets/script/common.js
@@ -1,34 +1,56 @@
+// const loadingElement = document.querySelector('.loading');
+// loadingElement.style.display = 'flex';
+
+// const loading = {
+//     init: function () {
+//         window.addEventListener('load', function () {
+//             // 스피너 확인 용 코드
+//             setTimeout(() => {
+//                 loadingElement.style.display = 'none';
+//             }, 11111500);
+//             // loadingElement.style.display = 'none';
+//         });
+//     },
+// };
+// loading.init();
+
 document.addEventListener('DOMContentLoaded', function () {
     // VARIABLES
     let ww = window.outerWidth;
     let st = window.scrollY;
     let isScrolling = false;
 
-    // $ELEMENTS
+    // CACHING
     const $header = document.querySelector('.header');
+    const $footer = document.querySelector('.footer');
     const $customSelect = document.querySelectorAll('.custom-select');
     const $btnAllMenu = document.querySelector('.btn-allmenu');
+    const $floatingMenu = document.querySelector('.floating-menu__container');
+    const $topButton = document.querySelector('.btn-top');
 
     // FUNCTIONS
     const scroll = {
-        calcScrollTop: function () {
+        updateScrollPosition() {
             st = window.scrollY;
-        },
-        headerToggle: function () {
-            if ($header) {
-                $header.classList.toggle('active', window.scrollY > 0);
-            }
-        },
-        handleScroll: function () {
             this.headerToggle();
-            this.calcScrollTop();
+            this.handleFloatingMenu();
         },
-        init: function () {
+        headerToggle() {
+            $header?.classList.toggle('active', st > 0);
+        },
+        handleFloatingMenu() {
+            const footerTop = $footer.offsetTop;
+            const screenBottom = st + window.innerHeight;
+
+            $floatingMenu.classList.toggle('fixed', screenBottom >= footerTop);
+            $floatingMenu.classList.toggle('active', st > 0);
+        },
+        init() {
             const checkScrolling = () => {
                 if (!isScrolling) {
                     isScrolling = true;
                     requestAnimationFrame(() => {
-                        this.handleScroll();
+                        this.updateScrollPosition();
                         isScrolling = false;
                     });
                 }
@@ -39,72 +61,95 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     const resize = {
-        calcScreenWidth: function () {
+        updateWindowWidth() {
             ww = window.outerWidth;
+            console.log(ww);
         },
-        handleResize: function () {
-            this.calcScreenWidth();
-        },
-        init: function () {
-            window.addEventListener('resize', this.handleResize.bind(this));
+        init() {
+            window.addEventListener('resize', this.updateWindowWidth.bind(this));
         },
     };
 
     const header = {
-        allMenuToggle: function () {
+        init() {
             if ($btnAllMenu) {
                 $btnAllMenu.addEventListener('click', function () {
                     this.classList.toggle('active');
                 });
             }
         },
-        init: function () {
-            this.allMenuToggle();
-        },
     };
 
     const customSelect = {
-        toggleCustomSelect: function () {
+        initSubMenu($subMenu) {
+            $subMenu.style.height = '0';
+            $subMenu.style.visibility = 'hidden';
+        },
+        toggleCustomSelect() {
             $customSelect.forEach(($select) => {
                 const $subMenu = $select.querySelector('ul');
-                if ($subMenu) {
-                    $subMenu.style.maxHeight = '0';
-                    $subMenu.style.overflow = 'hidden';
-                }
+                if ($subMenu) this.initSubMenu($subMenu);
 
-                $select.addEventListener('click', function (event) {
+                $select.addEventListener('click', (event) => {
                     event.stopPropagation();
-                    const siblings = Array.from($select.parentElement.children).filter((child) => child !== $select);
-                    this.classList.toggle('active');
-
-                    siblings.forEach((node) => {
-                        node.classList.remove('active');
-                        const $subMenu = node.querySelector('ul');
-                        if ($subMenu) {
-                            $subMenu.style.maxHeight = '0';
-                        }
-                    });
-
-                    if ($subMenu) {
-                        $subMenu.style.maxHeight = this.classList.contains('active') ? $subMenu.scrollHeight + 'px' : '0';
-                    }
+                    this.handleCustomSelect($select);
                 });
             });
         },
-        closeCustomSelect: function (event) {
+        handleCustomSelect($select) {
+            const siblings = Array.from($select.parentElement.children).filter((child) => child !== $select);
+            $select.classList.toggle('active');
+
+            siblings.forEach((node) => {
+                node.classList.remove('active');
+                const siblingSubMenu = node.querySelector('ul');
+                if (siblingSubMenu) this.hideSubMenu(siblingSubMenu);
+            });
+
+            const currentSubMenu = $select.querySelector('ul');
+            if (currentSubMenu) {
+                this[$select.classList.contains('active') ? 'showSubMenu' : 'hideSubMenu'](currentSubMenu);
+            }
+        },
+        showSubMenu(subMenu) {
+            subMenu.style.visibility = 'visible';
+            subMenu.style.height = `${subMenu.scrollHeight}px`;
+        },
+        hideSubMenu(subMenu) {
+            subMenu.style.height = '0';
+            subMenu.addEventListener(
+                'transitionend',
+                () => {
+                    if (subMenu.style.height === '0px') {
+                        subMenu.style.visibility = 'hidden';
+                    }
+                },
+                { once: true }
+            );
+        },
+        closeCustomSelect(event) {
             $customSelect.forEach(($select) => {
                 if (!$select.contains(event.target)) {
                     $select.classList.remove('active');
                     const $subMenu = $select.querySelector('ul');
-                    if ($subMenu) {
-                        $subMenu.style.maxHeight = '0';
-                    }
+                    if ($subMenu) this.hideSubMenu($subMenu);
                 }
             });
         },
-        init: function () {
+        init() {
             this.toggleCustomSelect();
             document.addEventListener('click', this.closeCustomSelect.bind(this));
+        },
+    };
+
+    const footer = {
+        scrollTop() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        },
+        init() {
+            if ($topButton) {
+                $topButton.addEventListener('click', this.scrollTop.bind(this));
+            }
         },
     };
 
@@ -113,4 +158,5 @@ document.addEventListener('DOMContentLoaded', function () {
     resize.init();
     header.init();
     customSelect.init();
+    footer.init();
 });
